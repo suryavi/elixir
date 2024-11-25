c3480e8aba718d892f1afdbebc3aa3c0d2d4725d
suryavi <suryaplaystore325@gmail.com>
2024-11-25 08:51:38 +0530

    src: init setup

---
+++ lib/mix_explore.ex
@@ -0,0 +1,18 @@
+defmodule MixExplore do
+  @moduledoc """
+  Documentation for `MixExplore`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MixExplore.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mix_explore_test.exs
@@ -0,0 +1,8 @@
+defmodule MixExploreTest do
+  use ExUnit.Case
+  doctest MixExplore
+
+  test "greets the world" do
+    assert MixExplore.hello() == :world
+  end
+end
